65d029312841f2abb3129e2b90ff468e3f2f1fa1
Toothless <max.rose366@gmail.com>
2016-07-13 16:05:28 +0100

    Pipes Now Connect in all Directions

--- PipeSystems/Assets/Scripts/ConnectPipe.cs
+++ PipeSystems/Assets/Scripts/ConnectPipe.cs
@@ -61,6 +61,25 @@ namespace Pipes
                     spawnedNub.transform.SetParent(transform);
                 }
             }
+
+            if (Physics.Raycast(transform.position, transform.right, out hit, 1))
+            {
+                if (hit.transform.tag == "Pipe")
+                {
+                    spawnedNub = (GameObject)Instantiate(pipeNub, thisPipesPositon + (Vector3.right * 0.35f), Quaternion.Euler(0, 90, 0));
+                    spawnedNub.transform.SetParent(transform);
+                }
+            }
+
+            if (Physics.Raycast(transform.position, (transform.right * -1), out hit, 1))
+            {
+
+                if (hit.transform.tag == "Pipe")
+                {
+                    spawnedNub = (GameObject)Instantiate(pipeNub, thisPipesPositon + (Vector3.left * 0.35f), Quaternion.Euler(0, 90, 0));
+                    spawnedNub.transform.SetParent(transform);
+                }
+            }
         }
     }
 }
