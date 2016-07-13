6f82663c1df393699f35600da7b3d110ea4a5cbd
Toothless <max.rose366@gmail.com>
2016-07-13 15:58:51 +0100

    Added Pipes that connect together

---
+++ PipeSystems/Assets/Scripts/ConnectPipe.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Pipes
+{
+    public class ConnectPipe : MonoBehaviour
+    {
+        public GameObject pipeNub;
+        private RaycastHit hit;
+        private Vector3 thisPipesPositon;
+        private GameObject spawnedNub;
+
+        // Use this for initialization
+        void Start()
+        {
+            thisPipesPositon = transform.position;
+            CheckForPipes();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void CheckForPipes()
+        {
+            if(Physics.Raycast(transform.position, transform.forward, out hit, 1))
+            {
+                if(hit.transform.tag == "Pipe")
+                {
+                    spawnedNub = (GameObject)Instantiate(pipeNub, thisPipesPositon + (Vector3.forward * 0.35f), Quaternion.Euler(0, 0, 0));
+                    spawnedNub.transform.SetParent(transform);
+                }
+            }
+
+            if (Physics.Raycast(transform.position, (transform.forward * -1), out hit, 1))
+            {
+                if (hit.transform.tag == "Pipe")
+                {
+                    spawnedNub = (GameObject)Instantiate(pipeNub, thisPipesPositon + -(Vector3.forward * 0.35f), Quaternion.Euler(0, 0, 0));
+                    spawnedNub.transform.SetParent(transform);
+                }
+            }
+            
+            if (Physics.Raycast(transform.position, transform.up, out hit, 1))
+            {
+                if (hit.transform.tag == "Pipe")
+                {
+                    spawnedNub = (GameObject)Instantiate(pipeNub, thisPipesPositon + (Vector3.up * 0.35f), Quaternion.Euler(90, 0, 0));
+                    spawnedNub.transform.SetParent(transform);
+                }
+            }
+
+            if (Physics.Raycast(transform.position, (transform.up * -1), out hit, 1))
+            {
+
+                if (hit.transform.tag == "Pipe")
+                {
+                    spawnedNub = (GameObject)Instantiate(pipeNub, thisPipesPositon + -(Vector3.up * 0.35f), Quaternion.Euler(90, 0, 0));
+                    spawnedNub.transform.SetParent(transform);
+                }
+            }
+        }
+    }
+}
